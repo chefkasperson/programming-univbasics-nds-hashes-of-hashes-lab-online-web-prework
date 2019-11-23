77a9f88a2c00f2be78edb357ca890e40a5d4fab9
Erik Kasperson <chefkasperson@gmail.com>
2019-11-23 08:48:11 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -34,15 +34,21 @@ def naming_system
   
   hash = {
     {:label => "Kingdom",
-    {:label => "Phylum"},
-    {:label => "Class"}
+    :sub_category => {
+      :label => "Phylum"
+    },
+    :sub_category => {
+      :label => "Class"
+    }
     },
     {:label => "Order"},
     {:label => "Family",
-    {:label => "Genus"}
+    :sub_category => {
+      :label => "Genus"
+    }
     },
     {:label => "Species",
-    {:sub_category => {}}
+    :sub_category => {}}
     }
   
   
